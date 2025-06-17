744ec90cf97d346e6767613e8a51547bba61a9e7
amaanlari <amaanlari@outlook.comm>
2025-06-17 07:54:50 +0530

    update package.json and vite.config.ts for deployment; remove gh-pages dependency and adjust base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mujtama-global-web',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
