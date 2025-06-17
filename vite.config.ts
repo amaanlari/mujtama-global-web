0a5902c0a76745ebafc0c36ecb5d6e128c2f2b3e
amaanlari <amaanlari@outlook.comm>
2025-06-17 07:41:43 +0530

    minor updates for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mujtama-global-web/',
+  base: '/mujtama-global-web',
   plugins: [react(), tailwindcss()],
 })
